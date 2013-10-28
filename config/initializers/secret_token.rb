29c8ec20f1c86ba4a6a16170a05f55998f77fd81
Julian Sutherland <juliansutherlandllc@gmail.com>
2013-10-28 15:32:53 -0400

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DyoLayoff::Application.config.secret_key_base = 'e448e3b021a763bac0f6930b658442cf1ca3016c1c8501f9a5b5ff6d4c20630164101a63dfcce72120b0511ec89c99135cc781926765a83a15df69534f5a8911'
